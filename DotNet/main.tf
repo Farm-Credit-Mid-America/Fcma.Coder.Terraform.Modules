b6424ca9041c00e0d36f45a0fe7e6b07d7af4783
fcma-sheigl <141864997+fcma-sheigl@users.noreply.github.com>
2024-10-16 15:50:17 -0400

    Create main.tf

---
+++ DotNet/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    coder = {
+      source  = "coder/coder"
+      version = ">= 0.17"
+    }
+  }
+}
+
+variable "agent_id" {
+  type        = string
+  description = "The ID of a Coder agent."
+}
+
+variable "install_version" {
+  type        = string
+  description = "The version of dotnet to install."
+  default     = "8.0"
+}
+
+resource "coder_script" "dotnet" {
+  agent_id     = var.agent_id
+  display_name = "dotnet"
+  icon         = "https://avatars.githubusercontent.com/u/9011267?v=4"
+  script = templatefile("${path.module}/run.sh", {
+    INSTALL_VERSION = var.install_version
+  })
+  run_on_start = true
+}
